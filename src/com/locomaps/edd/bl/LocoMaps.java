127fd1cbf6e073023d65ab1c75bb6a803e5a71a2
nmalesic <nicolas.malesic@Berger-Levraul.fr>
2016-01-18 15:27:56 +0100

    Récupération coordonnée Google dans la servlet

--- src/com/locomaps/edd/bl/LocoMaps.java
+++ src/com/locomaps/edd/bl/LocoMaps.java
@@ -1,6 +1,7 @@
 package com.locomaps.edd.bl;
 
 import java.io.IOException;
+
 import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
@@ -11,8 +12,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.locomaps.edd.bl.model.GoogleGeoCodeResponse;
+import com.locomaps.edd.bl.model.Latlng;
 import com.locomaps.edd.bl.model.MapsUtils;
 import com.locomaps.edd.bl.model.User;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 
 /**
  * Servlet implementation class Identification
@@ -50,11 +59,31 @@ public class LocoMaps extends HttpServlet {
 
 		String origin = request.getParameter("origin");
 		String waypoint = request.getParameter("waypoint1")+"|"+request.getParameter("waypoint2")+"|"+request.getParameter("waypoint3");
-		
-		
-		//User userSession = null;
-		
 
+		
+		String result = request.getParameter("JFM_RESULT");
+		String coords = request.getParameter("coords");
+		String coordslat = request.getParameter("coordslat");
+		String coordslng = request.getParameter("coordslng");
+
+	  	Latlng coordLatlng = null;
+	  	GoogleGeoCodeResponse gsonCoords = null;
+		
+	   // Récupération uniquement de la coordonnée
+//		  if(coords != null) {
+//			    final GsonBuilder gsonBuilder = new GsonBuilder();
+//			    final Gson gson = gsonBuilder.create();
+//			    coordLatlng = gson.fromJson( coords, Latlng.class);
+//		  }
+		  
+	  	// Récupération complète des info de la coordonnées
+		  if(result != null) {
+			    
+			    final GsonBuilder gsonBuilder = new GsonBuilder();
+			    final Gson gson = gsonBuilder.create();
+			    gsonCoords = gson.fromJson( result, GoogleGeoCodeResponse.class);
+		  }		  
+		  
 		HttpSession sessionScope = request.getSession();
 		String frame = MapsUtils.composeItineraire(origin,"64+rue+jean+rostand+31670+Labege",waypoint);
 		request.setAttribute("frame", frame);
@@ -62,39 +91,6 @@ public class LocoMaps extends HttpServlet {
 		User userSession = GestionSession.getUserSession(sessionScope);
 
 		this.getServletContext().getRequestDispatcher(VIEW_PAGES_URL).forward(request,response);
-		//sessionScope.getAttribute("userSession");
-		
-		/*RequestDispatcher dispat =	request.getRequestDispatcher("/accueil");
-		dispat.forward(request,response);
-		*/
-		
-//		if (login.equals("sylvain") ){
-//			RequestDispatcher dispat =	request.getRequestDispatcher("/accueil");
-//			dispat.forward(request,response);
-//		}
-//		else{
-//			doGet(request, response);
-//		}
-		
-		
-		
-//		if (userSession == null){
-//			// L'utilisateur n'existe pas dans la session
-//			doGet(request, response);
-//			
-//		} else {
-//			// L'utilisateur existe dans la session
-//			// Test du password
-//
-//			if (passwd.equals(userSession.getPassword())){
-//				sessionScope.setAttribute("userSession", userSession);
-//				RequestDispatcher dispat =	request.getRequestDispatcher("/accueil");
-//				dispat.forward(request,response);
-//			} else {
-//				// Le mot de passe est incorrect
-//			}
-//				doGet(request, response);
-//			}
 
 		}
 
---
+++ src/com/locomaps/edd/bl/model/Latlng.java
@@ -0,0 +1,6 @@
+package com.locomaps.edd.bl.model;
+
+public class Latlng {
+	public String lat;
+	public String lng;
+}
