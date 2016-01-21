35faeb7dea75552b2e8d70fb69a174bd9430d5e1
fcoeuret <fabrice.coeuret@berger-levrault.fr>
2016-01-21 13:34:13 +0100

    Fin tests connexionDB

--- src/com/locomaps/edd/bl/DB/ConnexionDB.java
+++ src/com/locomaps/edd/bl/DB/ConnexionDB.java
@@ -7,14 +7,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import com.locomaps.edd.bl.model.Adresse2D;
 import com.locomaps.edd.bl.model.GoogleGeoCodeResponse;
+import com.locomaps.edd.bl.model.Location;
 import com.locomaps.edd.bl.model.User;
-
-import fcoe.com.tetris.Connexion.ConnexionDBTetris;
-import fcoe.com.tetris.Connexion.Score;
 
 public class ConnexionDB {
 
@@ -22,6 +21,8 @@ public class ConnexionDB {
 	private static Statement statement;
 	private static Connection connection;
 	private static ConnexionDB connexionDB = null;
+	//private static String cheminBase = "C:/Users/fcoeuret/Documents/workspace-sts-3.7.0.RELEASE/LocoMaps/WebContent/DB/DB_LocoMaps.db";
+	private String cheminBase = "WebContent/DB/DB_LocoMaps.db";
 
 	public String getChaineConnection() {
 		return chaineConnection;
@@ -54,6 +55,14 @@ public class ConnexionDB {
 	public static void setConnexionTetris(ConnexionDB connexionDB) {
 		ConnexionDB.connexionDB = connexionDB;
 	}
+	
+	public String getcheminBase() {
+		return cheminBase;
+	}
+
+	public void setcheminBase(String cheminBase) {
+		this.cheminBase = cheminBase;
+	}
 
 	public ConnexionDB() {
 		// TODO Auto-generated constructor stub
@@ -72,7 +81,8 @@ public class ConnexionDB {
 			try 
 			{
 				//Connexion à la base de données
-				connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/fcoeuret/Documents/workspace-sts-3.7.0.RELEASE/TetrisGame/BDD/Tetris.db");
+				//connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/fcoeuret/Documents/workspace-sts-3.7.0.RELEASE/TetrisGame/BDD/Tetris.db");
+				connection = DriverManager.getConnection("jdbc:sqlite:" + cheminBase);
 				statement = connection.createStatement();
 				statement.setQueryTimeout(30);
 				
@@ -80,23 +90,20 @@ public class ConnexionDB {
 			catch (SQLException e) 
 			{
 				// TODO Auto-generated catch block
-				e.printStackTrace();
 				ok = false;
-			}
-			finally 
-			{
-				return ok;
+				e.printStackTrace();
 			}
 		}
+		return ok;
 	}
 
 	/**
 	 * Liste de tous les utilisateurs
 	 * @return Une liste de tous les utilisteurs
 	 */
-	public ArrayList<User> listAllUser()
+	public HashMap<String, User> listAllUser()
 	{
-		ArrayList<User> listUser = new ArrayList<User>();
+		HashMap<String, User> listUser = new HashMap<String, User>();
 
 		String rq = "SELECT * FROM UTILISATEUR LEFT JOIN UTILISATEUR.IDUTILISATEUR = ADRESSE.IDUTILISATEUR";
 		try 
@@ -107,20 +114,21 @@ public class ConnexionDB {
 			{
 				i++;
 				User user = new User();
-				user.setNomUtil(res.getString(2));
-				user.setPrenomUtil(res.getString(3));
-				user.setPseudo(res.getString(4));
-				user.setEmail(res.getString(5));
-				user.setPassword(res.getString(6));
-				user.setSexe(res.getString(7));
-				user.setFumeur(res.getString(8));
-				GoogleGeoCodeResponse gcoord = new GoogleGeoCodeResponse();
-				gcoord = res.getString(14);
-				Adresse2D adresse = new Adresse2D(res.getString(10),res.getString(11),res.getString(12),res.getString(13),gcoord);
-				user.setAddress(adresse);
-				listUser.add(user);
+				user = initUser(res);
+//				user.setId(res.getInt(1));
+//				user.setNomUtil(res.getString(2));
+//				user.setPrenomUtil(res.getString(3));
+//				user.setPseudo(res.getString(4));
+//				user.setEmail(res.getString(5));
+//				user.setPassword(res.getString(6));
+//				user.setSexe(res.getString(7));
+//				user.setFumeur(res.getString(8));
+//				GoogleGeoCodeResponse gcoord = new GoogleGeoCodeResponse();
+//				Adresse2D adresse = new Adresse2D(res.getString(10),res.getString(11),res.getString(12),res.getString(13),gcoord,res.getString(14));
+//				user.setAddress(adresse);
+	            listUser.put( user.getEmail(),user);
+				//listUser.add(user);
 			}
-			
 		}
 		catch (SQLException e)
 		{
@@ -138,18 +146,16 @@ public class ConnexionDB {
 	public Boolean addUser(User user)
 	{
 		Boolean ok = true;
-//		if (user !== null)
-//		{
-//			idScore = getMaxIdScore();
-//		}
-//		idScore++;
-		//String rq = "INSERT INTO UTILISATEUR VALUES (" + idScore + ",'" + sc.getNomJoueur() + "'," + sc.getNbLigne() + "," + sc.getNiveau() + "," + sc.getScore() + "," + sc.getDateScore() + "," + idGrille + ")";
-		String rqUtil = "INSERT INTO UTILISATEUR VALUES (,'" + user.getNomUtil() + "'," + user.getPrenomUtil() + "," + user.getPseudo() + "," + user.getEmail() + "," + user.getPassword() + "," + user.getSexe() + "," + user.getFumeur() + ")";
-		String rqAdr = "INSERT INTO ADRESSE VALUES (,'" + user.getAddress().getAdresse1() + "'," + user.getAddress().getAdresse2() + "," + user.getAddress().getCP() + "," + user.getAddress().getVille() + "," + user.getAddress().getLocation().lat + "," + user.getAddress().getLocation().lng + "," + user.getAddress().getTypeAddress2D() + ")";
-		
+		String rqAdr = "";
 		try 
 		{
+			String rqUtil = "INSERT INTO UTILISATEUR VALUES (" + null + ",\"" + user.getNomUtil() + "\",\"" + user.getPrenomUtil() + "\",\"" + user.getPseudo() + "\",\"" + user.getEmail() + "\",\"" + user.getPassword() + "\",\"" + user.getSexe() + "\",\"" + user.getFumeur() + "\",\"" + user.getTelephone() + "\")";
 			statement.executeUpdate(rqUtil);
+			int idUser = getIdUserByEmail(user.getEmail());
+			if (idUser != 0)
+			{
+				rqAdr = "INSERT INTO ADRESSE VALUES (" + null + "," + idUser + ",\"" + user.getAddress().getAdresse1() + "\",\"" + user.getAddress().getAdresse2() + "\",\"" + user.getAddress().getCP() + "\",\"" + user.getAddress().getVille() + "\"," + user.getAddress().getLocation().lat + "," + user.getAddress().getLocation().lng + ",\"" + user.getAddress().getTypeAddress2D() + "\",\"" + user.getAddress().getResult() + "\")";
+			}
 			statement.executeUpdate(rqAdr);
 		}
 		catch (SQLException e) 
@@ -164,13 +170,79 @@ public class ConnexionDB {
 		}
 	}
 	
-	public User getUSerByEmail(String email)
+	/**
+	 * Recherche Identifiant User par son email 
+	 * @param email
+	 * @return idUser
+	 */
+	private int getIdUserByEmail(String email)
 	{
-		String rq = "SELECT * FROM UTILISATEUR LEFT JOIN UTILISATEUR.IDUTILISATEUR = ADRESSE.IDUTILISATEUR WHERE Email = " + email;
+		int idUser = 0;
+		String rq = "SELECT UTILISATEUR.IDUTILISATEUR FROM UTILISATEUR WHERE UTILISATEUR.Email = \"" + email + "\"";
 		try 
 		{
 			ResultSet res = statement.executeQuery(rq);
-			User user = new User();
+			idUser = res.getInt(1);
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			return idUser;
+		}
+	}
+	
+	/**
+	 * Recherche User par son email
+	 * @param email
+	 * @return User
+	 */
+	public User getUserByEmail(String email)
+	{
+		User user = new User();
+		String rq = "SELECT * FROM UTILISATEUR U LEFT JOIN ADRESSE A ON U.IDUTILISATEUR = A.IDUTILISATEUR WHERE U.Email = \"" + email + "\"";
+		try 
+		{
+			ResultSet res = statement.executeQuery(rq);
+			user = initUser(res);
+//			user.setId(res.getInt(1));
+//			user.setNomUtil(res.getString(2));
+//			user.setPrenomUtil(res.getString(3));
+//			user.setPseudo(res.getString(4));
+//			user.setEmail(res.getString(5));
+//			user.setPassword(res.getString(6));
+//			user.setSexe(res.getString(7));
+//			user.setFumeur(res.getString(8));
+//			GoogleGeoCodeResponse gcoord = new GoogleGeoCodeResponse();
+//			Adresse2D adresse = new Adresse2D(res.getString(10),res.getString(11),res.getString(12),res.getString(13),gcoord,res.getString(14));
+//			user.setAddress(adresse);
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			user = null;
+			e.printStackTrace();
+		}
+		finally
+		{
+			return user;
+		}
+	}
+	
+	/**
+	 * Renseigne User par rapport à un ResultSet d'une requête
+	 * @param res (ResultSet)
+	 * @return User
+	 */
+	private User initUser(ResultSet res)
+	{
+		User user = new User();
+		try 
+		{
+			user.setId(res.getInt(1));
 			user.setNomUtil(res.getString(2));
 			user.setPrenomUtil(res.getString(3));
 			user.setPseudo(res.getString(4));
@@ -178,20 +250,24 @@ public class ConnexionDB {
 			user.setPassword(res.getString(6));
 			user.setSexe(res.getString(7));
 			user.setFumeur(res.getString(8));
-			//String gcoord = new GoogleGeoCodeResponse();
-			String gcoord = res.getString(14);
-			Adresse2D adresse = new Adresse2D(res.getString(10),res.getString(11),res.getString(12),res.getString(13),(GoogleGeoCodeResponse) gcoord);
+			user.setTelephone(res.getString(9));
+			GoogleGeoCodeResponse gcoord = new GoogleGeoCodeResponse();
+			Location location = new Location();
+			location.setLat(res.getString(16));
+			location.setLng(res.getString(17));
+			Adresse2D adresse = new Adresse2D(res.getString(12),res.getString(13),res.getString(14),res.getString(15),gcoord,res.getString(19));
+			adresse.setLocation(location);
 			user.setAddress(adresse);
-			
-			
-		}
-		catch (SQLException e)
+		} 
+		catch (SQLException e) 
 		{
 			// TODO Auto-generated catch block
+			user = null;
 			e.printStackTrace();
 		}
-
+		return user;
 	}
+	
 
 	
 	
---
+++ src/com/locomaps/edd/bl/test/ConnexionDBTest.java
@@ -0,0 +1,269 @@
+package com.locomaps.edd.bl.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.locomaps.edd.bl.DB.ConnexionDB;
+import com.locomaps.edd.bl.model.Adresse2D;
+import com.locomaps.edd.bl.model.GoogleGeoCodeResponse;
+import com.locomaps.edd.bl.model.Location;
+import com.locomaps.edd.bl.model.TypeAddress;
+import com.locomaps.edd.bl.model.User;
+
+public class ConnexionDBTest {
+
+	private ConnexionDB connexionDB;
+	private Statement statement;
+	private TypeAddress typeAdresse;
+	private ArrayList<User> listUser;
+	private static String cheminBase = "WebContent/DB/DB_LocoMaps_Test.db";
+
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	/**
+	 * Initialise une liste de User pour tests
+	 */
+	private void initListUser()
+	{
+		listUser = new ArrayList<User>();
+		//User 1
+		User user = new User();
+		GoogleGeoCodeResponse gCoord = new GoogleGeoCodeResponse();
+		Location location = new Location();
+		user.setNomUtil("COEURET");
+		user.setPrenomUtil("Fabrice");
+		user.setPseudo("fcoeuret");
+		user.setEmail("fcoeuret@segilog.com");
+		user.setPassword("123456789");
+		user.setSexe("Masculin");
+		user.setFumeur("Non");
+		user.setTelephone("0233455521");
+		Adresse2D adresse = new Adresse2D("8 bis rue du petit midi","","72400","PREVAL",gCoord,"result");
+		location.setLat("100");
+		location.setLng("200");
+		adresse.setLocation(location);
+		user.setAddress(adresse);
+		listUser.add(user);
+		
+		//User 2
+		user = new User();
+		gCoord = new GoogleGeoCodeResponse();
+		location = new Location();
+		user.setNomUtil("MALESIC");
+		user.setPrenomUtil("Nicolas");
+		user.setPseudo("nmalesic");
+		user.setEmail("nicolas.malsic@berger-levrault.fr");
+		user.setPassword("dff56eee4");
+		user.setSexe("Masculin");
+		user.setFumeur("Non");
+		user.setTelephone("06.32.55.55.66");
+		adresse = new Adresse2D("Rue Ampère","Près de SEGILOG","59340","FRELINGHIEN",gCoord,"result");
+		location.setLat("300");
+		location.setLng("2500");
+		adresse.setLocation(location);
+		user.setAddress(adresse);
+		listUser.add(user);
+		
+		//User 3
+		user = new User();
+		gCoord = new GoogleGeoCodeResponse();
+		location = new Location();
+		user.setNomUtil("CHAMAYOU");
+		user.setPrenomUtil("Olivier");
+		user.setPseudo("olivier.chamayou");
+		user.setEmail("olivier.chamayou@berger-levrault.fr");
+		user.setPassword("e4t4ers_6f");
+		user.setSexe("Masculin");
+		user.setFumeur("Oui");
+		user.setTelephone("07-54-54-54-44");
+		adresse = new Adresse2D("Rue de chez lui","Dans sa maison","31000","TOULOUSE",gCoord,"result");
+		location.setLat("400");
+		location.setLng("700");
+		adresse.setLocation(location);
+		user.setAddress(adresse);
+		listUser.add(user);
+		
+		//User 4
+		user = new User();
+		gCoord = new GoogleGeoCodeResponse();
+		location = new Location();
+		user.setNomUtil("RABOIS");
+		user.setPrenomUtil("Sylvain");
+		user.setPseudo("sylvain.rabois");
+		user.setEmail("sylvain.rabois@berger-levrault.fr");
+		user.setPassword("64ez78d");
+		user.setSexe("Masculin");
+		user.setFumeur("Non");
+		user.setTelephone("");
+		adresse = new Adresse2D("Boulevard de l'Europe","","31320","CASTANET-TOLOSAN",gCoord,"result");
+		location.setLat("800");
+		location.setLng("650");
+		adresse.setLocation(location);
+		user.setAddress(adresse);
+		listUser.add(user);
+		
+		//User 5
+		user = new User();
+		gCoord = new GoogleGeoCodeResponse();
+		location = new Location();
+		user.setNomUtil("DECHERF");
+		user.setPrenomUtil("Floriane");
+		user.setPseudo("fdecherf");
+		user.setEmail("fdecherf@segilog.com");
+		user.setPassword("pid4dsq6");
+		user.setSexe("Féminin");
+		user.setFumeur("Non");
+		user.setTelephone("0900000000");
+		adresse = new Adresse2D("Place de l église","","72600","ST VINCENT DES PRES",gCoord,"result");
+		location.setLat("1500");
+		location.setLng("750");
+		adresse.setLocation(location);
+		user.setAddress(adresse);
+		listUser.add(user);
+		
+	}
+	
+	@Test
+	public void initDBTest() {
+		
+		Boolean res = true;
+		connexionDB = new ConnexionDB();
+		connexionDB.setcheminBase(cheminBase);
+		Boolean ret = connexionDB.initDB();
+		assertEquals(res, ret);
+	}
+
+	private void deleteBase()
+	{
+		connexionDB = new ConnexionDB();
+		Boolean retConnexion = connexionDB.initDB();
+		connexionDB.setcheminBase(cheminBase);
+		if (retConnexion)
+		{
+			try 
+			{
+				String rqUtil = "DELETE FROM UTILISATEUR";
+				connexionDB.getStatement().executeUpdate(rqUtil);
+				String rqAdr = "DELETE FROM ADRESSE";
+				connexionDB.getStatement().executeUpdate(rqAdr);
+			}
+			catch (SQLException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally 
+			{
+				//return ok;
+			}
+		}
+	}
+	
+	@Test
+	public void addUserTest()
+	{
+		initListUser();
+		connexionDB = new ConnexionDB();
+		Boolean retConnexion = connexionDB.initDB();
+		connexionDB.setcheminBase(cheminBase);
+		Boolean retUser = false;
+		if (retConnexion)
+		{
+			deleteBase();
+			User user = new User();
+			user = listUser.get(3);
+			retUser = connexionDB.addUser(user);
+			assertEquals(true, retUser);
+			user = new User();
+			user = listUser.get(4);
+			retUser = connexionDB.addUser(user);
+			assertEquals(true, retUser);
+
+		}
+	
+	}
+	
+	@Test
+	public void getUserByEmailTest()
+	{
+		initListUser();
+		connexionDB = new ConnexionDB();
+		connexionDB.setcheminBase(cheminBase);
+		Boolean retConnexion = connexionDB.initDB();
+		Boolean retUser = false;
+		if (retConnexion)
+		{
+			deleteBase();
+			User user = new User();
+			user = listUser.get(1);
+			retUser = connexionDB.addUser(user);
+			User userRet = new User();
+			userRet = connexionDB.getUserByEmail(user.getEmail());
+			assertUser(user,userRet);
+		}
+	}
+	
+	private void assertUser(User user, User userRet)
+	{
+		assertEquals(user.getNomUtil(),userRet.getNomUtil());
+		assertEquals(user.getPrenomUtil(),userRet.getPrenomUtil());
+		assertEquals(user.getPseudo(),userRet.getPseudo());
+		assertEquals(user.getEmail(),userRet.getEmail());
+		assertEquals(user.getPassword(),userRet.getPassword());
+		assertEquals(user.getSexe(),userRet.getSexe());
+		assertEquals(user.getFumeur(),userRet.getFumeur());
+		assertEquals(user.getTelephone(),userRet.getTelephone());
+		assertEquals(user.getAddress().getAdresse1(),userRet.getAddress().getAdresse1());
+		assertEquals(user.getAddress().getAdresse2(),userRet.getAddress().getAdresse2());
+		assertEquals(user.getAddress().getCP(),userRet.getAddress().getCP());
+		assertEquals(user.getAddress().getVille(),userRet.getAddress().getVille());
+		assertEquals(user.getAddress().getResult(),userRet.getAddress().getResult());
+		assertEquals(user.getAddress().getLocation().getLat(),userRet.getAddress().getLocation().getLat());
+		assertEquals(user.getAddress().getLocation().getLng(),userRet.getAddress().getLocation().getLng());
+	}
+	
+	@Test
+	public void listAllUserTest()
+	{
+		HashMap<String, User> mapUser = new HashMap<String, User>();
+		HashMap<String, User> mapUserRet = new HashMap<String, User>();
+		User user = null;
+		initListUser();
+		connexionDB = new ConnexionDB();
+		connexionDB.setcheminBase(cheminBase);
+		Boolean retConnexion = connexionDB.initDB();
+		Boolean retUser = false;
+		if (retConnexion)
+		{
+			deleteBase();
+			Iterator<User> it = listUser.iterator();
+			while (it.hasNext())
+			{
+				user = new User();
+				user = (User) it.next();
+				mapUser.put(user.getEmail(), user);
+				retUser = connexionDB.addUser(user);
+			}
+			
+			mapUserRet = connexionDB.listAllUser();
+			while (it.hasNext())
+			{
+				user = new User();
+				assertUser(mapUser.get(user.getEmail()),mapUserRet.get(user.getEmail()));
+			}
+		}	
+	}
+	
+}
