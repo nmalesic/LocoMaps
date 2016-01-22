d762745adfd59d116b2490630a075ec8b461784d
Olivier <olivier.chamayou@berger-levrault.fr>
2016-01-22 11:18:45 +0100

    Correction BDD et PersistanceParameter

--- src/com/locomaps/edd/bl/model/bd/PersistanceParameter.java
+++ src/com/locomaps/edd/bl/model/bd/PersistanceParameter.java
@@ -5,13 +5,12 @@ import javax.servlet.http.HttpSession;
 public class PersistanceParameter {
 
 	public static DataProvider datatype = DataProvider.SQLITE;
-<<<<<<< HEAD
-	public static String chaineDeConnexion = "jdbc:sqlite:/WebContent/DB/DB_LocoMaps.db";
-=======
+
+	public static String chaineDeConnexion = "jdbc:sqlite:D:/DB/DB_LocoMaps.db";
+
 	
-	public static String chaineDeConnexion = "jdbc:sqlite:/App/sts-bundle/pivotal-tc-server-developer-3.1.1.RELEASE/base-instance/wtpwebapps/LocoMaps/DB/DB_LocoMaps.db";
+//	public static String chaineDeConnexion = "jdbc:sqlite:/App/sts-bundle/pivotal-tc-server-developer-3.1.1.RELEASE/base-instance/wtpwebapps/LocoMaps/DB/DB_LocoMaps.db";
 //	public static String chaineDeConnexion = "jdbc:sqlite:WebContent/DB/DB_LocoMaps.db";
->>>>>>> aeb9218a2a05c639e2bc1422517ac33ec501b6e8
 
 	
 }
